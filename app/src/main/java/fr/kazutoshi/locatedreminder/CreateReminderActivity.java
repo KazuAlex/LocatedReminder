6696b2679ad219faff804840c8fb48695bf01b91
alex <alex@debian-alex.home>
2015-12-20 15:06:52 +0100

    animation options CreateRemniderOptions -- toujours pb de edit text vide)

--- app/src/main/java/fr/kazutoshi/locatedreminder/CreateReminderActivity.java
+++ app/src/main/java/fr/kazutoshi/locatedreminder/CreateReminderActivity.java
@@ -1,6 +1,9 @@
 package fr.kazutoshi.locatedreminder;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Address;
@@ -15,10 +18,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -32,19 +37,30 @@ public class CreateReminderActivity extends AppCompatActivity {
 
   private MapFragment fragment;
   private AlarmHelper alarm;
+	private boolean optionsIsShowing;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_map_fragment);
-	if (getIntent().hasExtra("ALARM_ID")) {
-	  alarm = AlarmHelper.getFromId(getIntent().getLongExtra("ALARM_ID", 0));
-	  if (alarm != null) {
-	    EditText editTextName = (EditText) findViewById(R.id.alarmName);
-	    editTextName.setText(alarm.getRawName());
-	  }
-	}
+		if (getIntent().hasExtra("ALARM_ID")) {
+			RelativeLayout reminderOptions = (RelativeLayout) findViewById(R.id.reminderOptions);
+			alarm = AlarmHelper.getFromId(getIntent().getLongExtra("ALARM_ID", 0));
+			Log.d("locatedreminder", "alarm_id : " + alarm.getId());
+			if (alarm != null) {
+				Log.d("locatedreminder", "alarm != null on create");
+				reminderOptions.setVisibility(View.VISIBLE);
+				EditText editTextName = (EditText) findViewById(R.id.alarmName);
+				editTextName.setText(alarm.getRawName(), EditText.BufferType.EDITABLE);
+				reminderOptions.setVisibility(View.GONE);
+			}
+		}
     addMapFragment(alarm);
+
+		optionsIsShowing = true;
+
+		if (alarm != null) {
+		}
 
     findViewById(R.id.addReminderButton).setOnClickListener(new View.OnClickListener() {
 	    @Override
@@ -53,34 +69,34 @@ public class CreateReminderActivity extends AppCompatActivity {
 		    if (marker != null) {
 			    LatLng latLng = marker.getPosition();
 			    EditText editTextName = (EditText) findViewById(R.id.alarmName);
-				if (alarm == null) {
-					alarm = new AlarmHelper(-1, editTextName.getText().toString(), latLng.latitude, latLng.longitude,
-							fragment.getRadius(), true).save();
-				} else {
-					alarm.setName(editTextName.getText().toString())
-							.setLocationX(latLng.latitude)
-							.setLocationY(latLng.longitude)
-							.setRadius(fragment.getRadius())
-							.save();
-				}
+					if (alarm == null) {
+						alarm = new AlarmHelper(-1, editTextName.getText().toString(), latLng.latitude, latLng.longitude,
+								fragment.getRadius(), true).save();
+					} else {
+						alarm.setName(editTextName.getText().toString())
+								.setLocationX(latLng.latitude)
+								.setLocationY(latLng.longitude)
+								.setRadius(fragment.getRadius())
+								.save();
+					}
 		    }
 		    finish();
 	    }
     });
 
     findViewById(R.id.decreaseRadius).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        fragment.decreaseRadius(1);
-      }
-    });
+			@Override
+			public void onClick(View v) {
+				fragment.decreaseRadius(1);
+			}
+		});
 
     findViewById(R.id.increaseRadius).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        fragment.increaseRadius(1);
-      }
-    });
+			@Override
+			public void onClick(View v) {
+				fragment.increaseRadius(1);
+			}
+		});
   }
 
 	private void addMapFragment(AlarmHelper alarm) {
@@ -154,11 +170,44 @@ public class CreateReminderActivity extends AppCompatActivity {
 			showSearchInput();
 			return true;
 		} else if (id == R.id.action_extend_infos) {
-			RelativeLayout reminderOptions = (RelativeLayout) findViewById(R.id.reminderOptions);
-			if (reminderOptions.getVisibility() == View.VISIBLE)
-				reminderOptions.setVisibility(View.GONE);
-			else
-				reminderOptions.setVisibility(View.VISIBLE);
+			final RelativeLayout reminderOptions = (RelativeLayout) findViewById(R.id.reminderOptions);
+			if (optionsIsShowing) {
+				//reminderOptions.setVisibility(View.GONE);
+				Log.d("locationreminder", "optionsIsShowing");
+				optionsIsShowing = !optionsIsShowing;
+				View view = this.getCurrentFocus();
+				if (view != null) {
+					InputMethodManager imm =
+							(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+				}
+				reminderOptions.animate().setListener(new AnimatorListenerAdapter() {
+					@Override
+					public void onAnimationEnd(Animator animation) {
+						super.onAnimationEnd(animation);
+						Log.d("locatedreminder", "onanimationend");
+						reminderOptions.setVisibility(View.GONE);
+					}
+				}).translationY(-reminderOptions.getHeight()).start();
+			} else {
+				//reminderOptions.setVisibility(View.VISIBLE);
+				reminderOptions.setY(-reminderOptions.getHeight());
+				reminderOptions.animate()
+						.setListener(new AnimatorListenerAdapter() {
+							@Override
+							public void onAnimationEnd(Animator animation) {
+								super.onAnimationEnd(animation);
+								Log.d("locatedreminder", "onanimationend");
+								optionsIsShowing = !optionsIsShowing;
+							}
+
+							@Override
+							public void onAnimationStart(Animator animation) {
+								super.onAnimationStart(animation);
+								reminderOptions.setVisibility(View.VISIBLE);
+							}
+						}).translationY(0).start();
+			}
 			return true;
 		}
 
--- app/src/main/java/fr/kazutoshi/locatedreminder/MapFragment.java
+++ app/src/main/java/fr/kazutoshi/locatedreminder/MapFragment.java
@@ -26,145 +26,154 @@ import fr.kazutoshi.locatedreminder.models.AlarmHelper;
  * Created by Alex on 14/12/2015.
  */
 public class MapFragment extends Fragment {
-    MapView mapView;
-    private AlarmHelper alarm;
-    private GoogleMap googleMap;
-    private Marker marker;
-    private Circle radiusCircle;
-    private boolean initialized = false;
-    private double radius;
+  MapView mapView;
+  private AlarmHelper alarm;
+  private GoogleMap googleMap;
+  private Marker marker;
+  private Circle radiusCircle;
+  private boolean initialized = false;
+  private double radius;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_map_fragment, container, false);
-        mapView = (MapView) v.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.onResume();
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.activity_map_fragment, container, false);
+    mapView = (MapView) v.findViewById(R.id.mapView);
+    mapView.onCreate(savedInstanceState);
+    mapView.onResume();
 
-        marker = null;
+    marker = null;
 
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
+    try {
+      MapsInitializer.initialize(getActivity().getApplicationContext());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    googleMap = mapView.getMap();
+    googleMap.setMyLocationEnabled(true);
+    LocationManager manager =
+            (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+    Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    locate(location);
+
+    radius = 20;
+    googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+        @Override
+        public void onMapLongClick(LatLng latLng) {
+          setMarkerLocation(latLng);
         }
+    });
 
-        googleMap = mapView.getMap();
-        googleMap.setMyLocationEnabled(true);
-        LocationManager manager =
-                (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
-        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locate(location);
-
-        radius = 20;
-        googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-	            setMarkerLocation(latLng);
-            }
-        });
-
-        if (alarm != null) {
-            radius = alarm.getRadius();
-            setMarkerLocation(new LatLng(alarm.getLocationX(), alarm.getLocationY()));
-        }
-
-        return v;
+    if (alarm != null) {
+        radius = alarm.getRadius();
+        setMarkerLocation(new LatLng(alarm.getLocationX(), alarm.getLocationY()), 17.0f);
     }
 
-    public void setMarkerLocation(LatLng latLng) {
-        if (marker != null) {
-            marker.remove();
-            radiusCircle.remove();
-        }
-        marker = googleMap.addMarker(
-                new MarkerOptions().position(latLng));
-	      centerOnLatLng(latLng);
-        radiusCircle = googleMap.addCircle(
-                new CircleOptions().center(latLng).radius(radius)
-                        .strokeColor(Color.argb(255, 66, 92, 151))
-                        .fillColor(Color.argb(30, 66, 92, 151))
-                        .strokeWidth(2));
+    return v;
+  }
+
+  public void setMarkerLocation(LatLng latLng) {
+    setMarkerLocation(latLng, null);
+  }
+
+  public void setMarkerLocation(LatLng latLng, Float zoom) {
+    if (marker != null) {
+      marker.remove();
+      radiusCircle.remove();
+    }
+    marker = googleMap.addMarker(
+        new MarkerOptions().position(latLng));
+    centerOnLatLng(latLng, zoom);
+
+    radiusCircle = googleMap.addCircle(
+          new CircleOptions().center(latLng).radius(radius)
+                .strokeColor(Color.argb(255, 66, 92, 151))
+                .fillColor(Color.argb(30, 66, 92, 151))
+                .strokeWidth(2));
+  }
+
+  private void locate(Location location) {
+    if (location != null)
+      if (!initialized) {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+            new LatLng(location.getLatitude(), location.getLongitude()), 17.0f));
+        initialized = true;
+      }
+  }
+
+  public MapFragment centerOnLatLng(LatLng latLng, Float zoom) {
+    if (latLng != null)
+      if (zoom == null) {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+      } else {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+      }
+    return this;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    mapView.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mapView.onPause();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+
+
+  public Marker getMarker() {
+      return marker;
+  }
+
+  public double getRadius() {
+    if (radiusCircle != null)
+      return radiusCircle.getRadius();
+    return 0;
+  }
+
+  public MapFragment increaseRadius(double plusplus) {
+      return changeRadius(plusplus);
+  }
+
+  public MapFragment decreaseRadius(double moinsmoins) {
+    return changeRadius(-moinsmoins);
+  }
+
+  public MapFragment changeRadius(double changechange) {
+    if (radiusCircle != null) {
+      if (radius + changechange < 0)
+        radius = 0;
+      else
+        radius += changechange;
+      radiusCircle.setRadius(radius);
+    }
+    return this;
+  }
+
+  public MapFragment setAlarm(AlarmHelper alarm) {
+    if (alarm != null) {
+      this.alarm = alarm;
+      /*radius = alarm.getRadius();
+      setMarkerLocation(new LatLng(alarm.getLocationX(), alarm.getLocationY()));*/
     }
 
-    private void locate(Location location) {
-        if (location != null)
-            if (!initialized) {
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                        new LatLng(location.getLatitude(), location.getLongitude()), 17.0f));
-                initialized = true;
-            }
-    }
-
-		public MapFragment centerOnLatLng(LatLng latLng) {
-			if (latLng != null)
-				googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-			return this;
-		}
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-
-
-    public Marker getMarker() {
-        return marker;
-    }
-
-    public double getRadius() {
-        if (radiusCircle != null)
-            return radiusCircle.getRadius();
-        return 0;
-    }
-
-    public MapFragment increaseRadius(double plusplus) {
-        return changeRadius(plusplus);
-    }
-
-    public MapFragment decreaseRadius(double moinsmoins) {
-        return changeRadius(-moinsmoins);
-    }
-
-    public MapFragment changeRadius(double changechange) {
-        if (radiusCircle != null) {
-            if (radius + changechange < 0)
-                radius = 0;
-            else
-                radius += changechange;
-            radiusCircle.setRadius(radius);
-        }
-        return this;
-    }
-
-    public MapFragment setAlarm(AlarmHelper alarm) {
-        if (alarm != null) {
-            this.alarm = alarm;
-            /*radius = alarm.getRadius();
-            setMarkerLocation(new LatLng(alarm.getLocationX(), alarm.getLocationY()));*/
-        }
-
-        return this;
-    }
+    return this;
+  }
 }
